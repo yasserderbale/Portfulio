da88cfda3bf1f50d99e89dc855ec94e6fc6611a7
yasserderbale <drbalyasser@gmaile.com>
2024-11-30 23:13:47 +0100

    update

--- Front-end/src/Compenentes/Aboute.jsx
+++ Front-end/src/Compenentes/Aboute.jsx
@@ -46,7 +46,7 @@ export const Aboute = () => {
             <button className="btn-contact">Download Cv</button>
           </a>
         </div>
-        <img className="  " src={about} alt="About" />
+        <img className="Aboutimg" src={about} alt="About" />
       </Container>
     </div>
   );
